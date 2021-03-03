f0cd662d1630f41fb19181f1012a71871d165b51
aczx131 <richard.renaud@city.co.uk>
2021-03-03 12:50:30 +0000

    ** exercise003.js ** Code implementation for 'getTotalSubjects'.
    Note
    Found a nice code snippet to return a unique list of subjects, routine was not required for the test case to pass but the wording of the test description implies that the code should return the number of UNIQUE subjects.  Music is taken by 2 students, but music is a single subject and should not be counted twice.

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -23,9 +23,31 @@ function camelCaseWords(words) {
   return camelStr.charAt(0).toLowerCase() + camelStr.slice(1);
 }
 
+/**
+ * Solution derived from url below:
+ * https://appdividend.com/2019/04/11/how-to-get-distinct-values-from-array-in-javascript/
+ * @param people
+ * @returns {number}
+ */
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  var subjectsArr = Array();
+  people.forEach((person) => { // Outter loop
+    person.subjects.forEach((subject) => {
+      subjectsArr.push(subject);  // add all subjects to Array List
+    });
+  });
+
+  /**
+   * Routine below will show number of unique subjects
+   * not every subject added to the list
+   */
+  // const unique = (value, index, self) => {
+  //   return self.indexOf(value) === index
+  // }
+
+  // return subjectsArr.filter(unique).length;
+  return subjectsArr.length;
 }
 
 function checkIngredients(menu, ingredient) {
