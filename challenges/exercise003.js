b971237a8aec38661165db274c021b92e2140f81
aczx131 <richard.renaud@city.co.uk>
2021-03-19 07:44:47 +0000

    ** exercise003.js ** Refactored exercises as per feedback from Kim and Ellie

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,10 +1,14 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  var squareNum = new Array();
+  let squareNum = new Array();
 
-  nums.forEach((n) => {
-    squareNum.push(n * n);
+  nums.map((n) => {
+    squareNum.push(n * n)
   });
+
+  // nums.forEach((n) => {
+  //   squareNum.push(n * n);
+  // });
 
   // I really like the algorithm using MAP from.
   //https://storage.googleapis.com/tech-returners-course/JavaScript_Challenges/JS_challenges_week3_squarenums_3ways.mp4
@@ -20,13 +24,15 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  var camelStr = ""; // temp string
+  let camelStr = ""; // temp string
 
-  words.forEach((w) => {  // loop all words
-    camelStr +=
-        words.length > 1 ? w.charAt(0).toUpperCase() + w.slice(1) // Capitalises word
-            : w; // return word NOT capitalised
-  });
+  words.map((w) => camelStr += words.length > 1 ? w.charAt(0).toUpperCase() + w.slice(1) : w);
+
+  // words.forEach((w) => {  // loop all words
+  //   camelStr +=
+  //       words.length > 1 ? w.charAt(0).toUpperCase() + w.slice(1) // Capitalises word
+  //           : w; // return word NOT capitalised
+  // });
 
   return camelStr.charAt(0).toLowerCase() + camelStr.slice(1);
 }
@@ -39,7 +45,8 @@ function camelCaseWords(words) {
  */
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  var subjectsArr = Array();
+  let subjectsArr = Array();
+
   people.forEach((person) => { // Outter loop
     person.subjects.forEach((subject) => {
       subjectsArr.push(subject);  // add all subjects to Array List
@@ -61,14 +68,18 @@ function getTotalSubjects(people) {
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  var hasIngredient = false;
+  let hasIngredient = false;
 
-  menu.forEach((dish) => {
-    // self assignment = [DO NOTHING]
-    // ingredients array needs to be sorted to satisfy test case
-    // eslint-disable-next-line no-self-assign
-    hasIngredient = dish.ingredients.includes(ingredient) ? true : hasIngredient = hasIngredient;
-  });
+  menu.map((dish) => hasIngredient = dish.ingredients.includes(ingredient) ? true
+      // eslint-disable-next-line no-self-assign
+      : hasIngredient = hasIngredient);
+
+  // menu.forEach((dish) => {
+  //   // self assignment = [DO NOTHING]
+  //   // ingredients array needs to be sorted to satisfy test case
+  //   // eslint-disable-next-line no-self-assign
+  //   hasIngredient = dish.ingredients.includes(ingredient) ? true : hasIngredient = hasIngredient;
+  // });
 
   return hasIngredient;
 }
@@ -76,11 +87,11 @@ function checkIngredients(menu, ingredient) {
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  var tmpArr = Array();
+  let tmpArr = Array();
 
   // if num in arr1 and NOT in tmpArr
   arr2.forEach((num) => {
-    arr1.includes(num) && !tmpArr.includes(num) ? tmpArr.push(num) : console.log("Do Nothing  " + num);
+    arr1.includes(num) && !tmpArr.includes(num) ? tmpArr.push(num) : null;
   });
 
   // Need to sort the output to satisfy test case
